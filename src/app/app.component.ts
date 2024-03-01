c9b9c6fb86b5af666977f9716ebf3bc687482851
sreenath <sreenathbr20@gmail.com>
2024-03-01 17:12:13 +0530

    Home page added with menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { RouterLink,RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule,CommonModule,RouterLink],
+  imports: [RouterOutlet,FormsModule,CommonModule,RouterLink,RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  isUserLogedIn:boolean=true;
 
  }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from '@angular/router';
 import { DemoComponent } from './components/demo/demo.component';
+import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
-    {path:'login',component:LoginComponent},
-    {path:'register',component:RegisterComponent},
-    {path:'demo',component:DemoComponent},
-    {path:'**',component:PageNotFoundComponent}
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'demo', component: DemoComponent },
+    { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: 'home', pathMatch:'full' },
+    { path: '**', component: PageNotFoundComponent }
 ];
